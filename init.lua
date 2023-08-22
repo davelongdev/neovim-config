58ccdd048368ac36fad8e12b4b8c30528e71a134
dave long <davelongdev@gmail.com>
2023-08-22 15:47:09 -0400

    add friendly snippits plugin

--- init.lua
+++ init.lua
@@ -144,6 +144,7 @@ require('lazy').setup({
     dependencies = {
       'hrsh7th/cmp-nvim-lsp',
       'L3MON4D3/LuaSnip',
+      dependencies = { 'rafamadriz/friendly-snippets' },
       'saadparwaiz1/cmp_luasnip' },
   },
 
