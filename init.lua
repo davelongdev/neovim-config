1cec8c4cce18dead6db1e3297f0c025d12cb6973
dave long <davelongdev@gmail.com>
2023-08-23 13:57:33 -0400

    update diagnostic config

--- init.lua
+++ init.lua
@@ -848,7 +848,7 @@ vim.diagnostic.config({
   virtual_text = false,
   signs = true,
   underline = true,
-  update_in_insert = true,
+  update_in_insert = false,
   severity_sort = true,
   float = {
     focusable = false,
