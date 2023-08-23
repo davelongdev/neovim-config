b438750777e92a241ba5c6295a1e0b2e685d48ea
dave long <davelongdev@gmail.com>
2023-08-23 13:57:18 -0400

    small refactor

--- init.lua
+++ init.lua
@@ -866,7 +866,7 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnos
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
--- LSP settings.
+-- LSP settings
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
   -- NOTE: Remember that lua is a real programming language, and as such it is possible
