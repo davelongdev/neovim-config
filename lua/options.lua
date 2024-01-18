e7202e9909aad184577de1133f91a477c2846930
dave long <davelongdev@gmail.com>
2024-01-18 12:09:50 -0500

    disable swap file for using tmux

--- lua/options.lua
+++ lua/options.lua
@@ -39,4 +39,6 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+-- Disable swapfile for using tmux
+vim.o.swapfile=false
 -- vim: ts=2 sts=2 sw=2 et
