0f8bc47f76e9dee3f95bb1b522997ebe03e35efe
dave long <davelongdev@gmail.com>
2024-01-19 16:02:29 -0500

    fix typeo in lsp-setup

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -48,7 +48,7 @@ require('which-key').register {
   ['<leader>c'] = { name = '[c]ode', _ = 'which_key_ignore' },
   ['<leader>d'] = { name = '[d]ocument', _ = 'which_key_ignore' },
   ['<leader>g'] = { name = '[g]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Gitg[h]unk', _ = 'which_key_ignore' },
+  ['<leader>h'] = { name = 'git [h]unk', _ = 'which_key_ignore' },
   ['<leader>r'] = { name = '[r]ename', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[s]earch', _ = 'which_key_ignore' },
   ['<leader>t'] = { name = '[t]oggle', _ = 'which_key_ignore' },
