cb37dae7c4b880a1bc96e4de3250ffd9684b316b
dave long <davelongdev@gmail.com>
2024-01-19 15:45:50 -0500

    update git status symbols for neo-tree

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -64,7 +64,23 @@ require('lazy').setup({
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+    },
+    config = function()
+      require('neo-tree').setup({
+        default_component_configs = {
+          git_status = {
+            symbols = {
+               -- Status type
+              untracked = "?",
+              ignored   = "i",
+              unstaged  = "U",
+              staged    = "S",
+              conflict  = "C",
+            }
+          }
+        }
+      })
+    end,
   },
 
 -- jsdoc automation for js comment/documentation generation
