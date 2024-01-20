db7e4295418decbdbb9bc6ba44422ee227e50ae0
dave long <davelongdev@gmail.com>
2024-01-19 23:46:55 -0500

    add neogit with basic setup

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -28,6 +28,19 @@ require('lazy').setup({
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
 
+{
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+
+    -- Only one of these is needed, not both.
+    "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua",              -- optional
+  },
+  config = true
+},
+
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
