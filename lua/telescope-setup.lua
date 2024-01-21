9f1d55f12ccf396bbc42ed51f0ce4da70ffb6250
dave long <davelongdev@gmail.com>
2024-01-21 15:59:40 -0500

    adjust telescope keymaps

--- lua/telescope-setup.lua
+++ lua/telescope-setup.lua
@@ -72,7 +72,7 @@ local function telescope_live_grep_open_files()
 end
 vim.keymap.set('n', '<leader>s/', telescope_live_grep_open_files, { desc = 'search [/] in open files' })
 vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = 'search [s]elect telescope' })
-vim.keymap.set('n', '<leader>Gf', require('telescope.builtin').git_files, { desc = 'search git [f]iles' })
+vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'search git [f]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = 'search [f]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = 'search [h]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = 'search current [w]ord' })
@@ -80,5 +80,6 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = 'search by [G]rep on git root' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = 'search [d]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 'search [r]esume' })
+vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = 'search [k]eymaps' })
 
 -- vim: ts=2 sts=2 sw=2 et
