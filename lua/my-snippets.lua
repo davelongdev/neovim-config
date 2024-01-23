4a5878d8d9d7215954efa7c327671c35c9456416
dave long <davelongdev@gmail.com>
2024-01-22 22:15:15 -0500

    update my-snippets.lua

--- lua/my-snippets.lua
+++ lua/my-snippets.lua
@@ -9,16 +9,19 @@ ls.add_snippets({
   'all', {
     ls.parser.parse_snippet(
       'hw',
-      'hello world'),
+      'hello world'
+    ),
   },
   'js', {
     ls.parser.parse_snippet(
       'cl',
-      'console.log($0)'),
+      'console.log($0)'
+    ),
   },
   'lua', {
     ls.parser.parse_snippet(
       'lf',
-      'local $1 = function($2)/n $0/nend'),
+      'local $1 = function($2)/n $0/nend'
+    ),
   }
 })
