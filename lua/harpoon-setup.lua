daf9a41c67b713fa5bc7d49cdab2676429fcdb2f
dave long <davelongdev@gmail.com>
2024-01-22 22:11:57 -0500

    update harpoon keymaps

--- lua/harpoon-setup.lua
+++ lua/harpoon-setup.lua
@@ -3,8 +3,8 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>W", mark.add_file, { desc = "add harpoon mark" })
-vim.keymap.set("n", "<M-w>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>b", mark.add_file, { desc = "add harpoon mark" })
+vim.keymap.set("n", "<C-b>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
