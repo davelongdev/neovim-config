a8400b2908f0ebf53a2ab233d96fabc3b30d79a6
dave long <davelongdev@gmail.com>
2024-01-24 12:55:33 -0500

    refactor code in neotree-setup to use unused variable

--- lua/neotree-setup.lua
+++ lua/neotree-setup.lua
@@ -1,6 +1,6 @@
 local nt = require 'neo-tree'
 
-require("neo-tree").setup({
+nt.setup({
   close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
   popup_border_style = "rounded",
   enable_git_status = true,
