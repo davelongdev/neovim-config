f1065ca03af9542e91d09c851e51f43f8e3c51b3
dave long <davelongdev@gmail.com>
2024-01-26 22:05:26 -0500

    adds repeat functionality with . for plugins

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -12,6 +12,9 @@ require('lazy').setup({
 
 -- [[ Misc plugins ]]
 
+-- adds repeat functionality with . for plugins
+  'tpope/vim-repeat',
+
 -- add window maximize functionality
   'szw/vim-maximizer',
 
