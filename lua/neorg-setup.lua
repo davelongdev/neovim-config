ac2e37133b3e6990e9eb3c92bce16cdac2fd5bbb
dave long <davelongdev@gmail.com>
2024-02-11 18:37:39 -0500

    update indentation and bullet formatting

--- lua/neorg-setup.lua
+++ lua/neorg-setup.lua
@@ -1,30 +1,53 @@
 require('neorg').setup {
-    load = {
+  load = {
 
-      -- loads defaults
-      ["core.defaults"] = {},
+    -- loads defaults
+    ["core.defaults"] = {},
 
-      -- conceals syntax code and adds icons
-      ["core.concealer"] = {
-        config = {
-        icon_preset = 'diamond'
+    -- conceals syntax code and adds icons
+    ["core.concealer"] = {
+      config = {
+      icon_preset = 'diamond'
       }
     },
 
-      -- manages neorg workspaces
-      ["core.dirman"] = {
-        config = {
-          workspaces = {
-            main = "~/neorg",
-            notes = "~/neorg/notes",
-          },
+    -- manages neorg workspaces
+    ["core.dirman"] = {
+      config = {
+        workspaces = {
+          main = "~/neorg",
+          notes = "~/neorg/notes",
         },
       },
+    },
 
     -- adds table of contents functionality
     ["core.qol.toc"] = {
       config = {
         close_after_use = true
+      }
+    },
+
+    -- sets indent level
+    ["core.esupports.indent"] = {
+      config = {
+        indents = {
+          heading2 = {
+            indent = 1
+          },
+          heading3 = {
+            indent = 2
+          },
+          heading4 = {
+            indent = 3
+          },
+          heading5 = {
+            indent = 4
+          },
+          heading6 = {
+            indent = 5
+          }
+        }
       }
     },
   }
